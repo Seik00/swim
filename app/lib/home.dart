d5c7b82cd59ff242c90aa30a806ecc7ec38556a2
Cjsong0817 <47886613+Cjsong0817@users.noreply.github.com>
2023-08-28 00:50:21 +0800

    chong

--- app/lib/home.dart
+++ app/lib/home.dart
@@ -58,6 +58,48 @@ class _HomepageState extends State<Homepage> {
                     ),
                   ),
                 ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: 1,
+                    itemBuilder: (context, int i) {
+                      return Container(
+                        margin: const EdgeInsets.only(top: 20),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 30, vertical: 12),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: const Color.fromRGBO(224, 228, 227, 1)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: const [
+                                Icon(Icons.calendar_month),
+                                Text(
+                                  'May 13, 2023 to May 14, 2023',
+                                  style: TextStyle(fontSize: 12),
+                                )
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                                ('Sportexcel junior swimming circuit 2023')
+                                    .toUpperCase(),
+                                style: const TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.w600)),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            const Text('SHAH ALAM, SEL MY',
+                                style: TextStyle(fontSize: 12)),
+                          ],
+                        ),
+                      );
+                    }),
               )
             ],
           ),
---
+++ app/lib/meets/program.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MeetProgram extends StatefulWidget {
+  const MeetProgram({super.key});
+
+  @override
+  State<MeetProgram> createState() => _MeetProgramState();
+}
+
+class _MeetProgramState extends State<MeetProgram> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
